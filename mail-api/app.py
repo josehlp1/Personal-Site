2fa5179aff28b4dc8e6a1318362393a2c66f6df9
José Henrique <jose.l.pellet@gmail.com>
2022-02-27 20:47:51 -0300

    Inicio Envio de Emails
    Erros a serem corrigidos

--- mail-api/app.py
+++ mail-api/app.py
@@ -1,21 +1,21 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api
-import json
-import smtplib
+import smtplib, ssl
 
 app = Flask(__name__)
+
 class SendMail(Resource):
     def post(self):
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.login("jose.l.pellet@gmail.com", "j1o2s3e4")
-        server.sendmail(
-            "jose.l.péllet@gmail.com",
-            "jose.datamais@gmail.com",
-            "Conteúdo da mensagem")
-        server.quit()
-
-
-        return {'nome': 'José'}
+        port = 465
+        smtp_server = "smtp.gmail.com"
+        sender_email = "jose.datamais@gmail.com"
+        receiver_email = "jose.l.pellet@gmail.com"
+        password = "j1j2j3j4j5"
+        message = "Teste"
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
 
 api = Api(app)
 
